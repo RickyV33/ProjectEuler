b1309bfdd72ec36b4391f655fbd5a33a679c0399
RickyV33 <3D.RickyV@gmail.com>
2014-09-06 21:40:58 -0700

    Add solution for problem 4 in Python

---
+++ Python/prob004.py
@@ -0,0 +1,22 @@
+def largestPalindromeProduct(min = 100, max = 1000):
+    """Finds the largest palindrome in the range of min and max.
+    min and max defaults to 100 and 1000 respectively if no argument is passed in."""
+    
+    palindrome = 0
+
+    for x in range(min, max):
+        for y in range(min, max):
+            product = x*y
+            if checkPalindrome(product) and palindrome < x*y:
+                palindrome = product
+    
+    print palindrome
+    
+def checkPalindrome(num):
+    """Checks if num is a palindrome. 
+    If num is a palindrome, return True, otherwise return False"""
+
+    return str(num) == str(num)[::-1] #Compares string num with a reversed string num
+
+if __name__ == "__main__":
+    largestPalindromeProduct()
