fa2e42bfa84a0c9ace23230f3d2d45c84e9463cf
RickyV33 <3D.RickyV@gmail.com>
2014-09-17 14:26:17 -0700

    Fix while loop in problem 7 in Python

--- Python/prob007.py
+++ Python/prob007.py
@@ -1,9 +1,11 @@
+import math
+
 def isPrime(num):
     """Returns True if num evaluates to a prime number, returns false otherwise."""
 
     divisor = 2 #Holds the divisor that num is divided by
 
-    while divisor < num:
+    while divisor < math.sqrt(num):
         if num % divisor == 0:
            return False
         else:
@@ -12,18 +14,17 @@ def isPrime(num):
     return True
 
 def findNthPrime(N):
-    """Finds the Nth prime number from 0.
-    Returns the Nth prime number if it exists, otherwise return 0"""
+    """Returns the Nth prime number."""
 
-    b = 2
+    prime = 2 #Holds the prime number
     counter = 1 #Keeps track of the prime count. Starts at 1 to count 2 as prime
 
     while counter < N:
-        if isPrime(b):
+        if isPrime(prime):
             counter += 1
-        b = 3 if b == 2 else b + 2 #Only uses odd numbers besides 2 (2,3,5,7,9...)
+        prime = 3 if prime == 2 else prime + 2 #Only uses odd numprimeers primeesides 2 (2,3,5,7,9...)
 
-    return b
+    return prime
 
 
 if __name__ == "__main__":
